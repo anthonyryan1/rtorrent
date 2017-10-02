ae1d46252c4da8149009fc4c40ca899fe4dcb8dc
Anthony Ryan <anthonyryan1@gmail.com>
2017-10-02 14:54:12 -0400

    Simplify curl dual stack IPv4 / IPv6 handling
    curl implements the RFC 6555 "Happy Eyeballs" algorithm, meaning a
    manual fallback to IPv6 is uncessary and arguably detrimental.
    
    If we leave curl to it's own devices with the request, it will attempt
    to connect on both IPv4 and IPv6, then use whichever connection is
    going to be more reliable.

--- src/core/curl_stack.cc
+++ src/core/curl_stack.cc
@@ -134,16 +134,9 @@ CurlStack::process_done_handle() {
 
   if (msg->data.result == CURLE_COULDNT_RESOLVE_HOST) {
     iterator itr = std::find_if(begin(), end(), rak::equal(msg->easy_handle, std::mem_fun(&CurlGet::handle)));
- 
+
     if (itr == end())
       throw torrent::internal_error("Could not find CurlGet when calling CurlStack::receive_action.");
- 
-    if (!(*itr)->is_using_ipv6()) {
-      (*itr)->retry_ipv6();
-
-      if (curl_multi_add_handle((CURLM*)m_handle, (*itr)->handle()) > 0)
-        throw torrent::internal_error("Error calling curl_multi_add_handle.");
-    }
 
   } else {
     transfer_done(msg->easy_handle,
